f7eecb09c02a3f4da9fc6603807af3c226a4f7c4
Herry-Xu <herryxu123@gmail.com>
2019-11-20 16:35:38 -0500

    Getting plotting to work

--- src/com/example/planner/MealPlanner.java
+++ src/com/example/planner/MealPlanner.java
@@ -1,5 +1,12 @@
 package com.example.planner;
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 enum RequestType {
@@ -9,17 +16,37 @@ enum RequestType {
 public class MealPlanner {
 
     private static void launchApp(){
-        double calories = -1;
+        List<Double> calories = new ArrayList<>();
         Scanner input = new Scanner(System.in);
+        double curCal;
         while (true) {
             System.out.println("Log a food item:");
             String food = input.next();
+            if (food.equals("done")){
+                break;
+            }
             try {
-                calories = HttpQueries.getFoods(food);
+                curCal = HttpQueries.getFoods(food);
+                calories.add(curCal);
+                System.out.printf("%s is %f calories\n\n", food, curCal);
             } catch (IOException e){
                 System.out.printf("Found error: %s", e);
             }
-            System.out.printf("%s is %f calories\n\n", food, calories);
+        }
+        plotChart(calories);
+    }
+
+    private static void plotChart(List<Double> calories){
+        double[] dateData = new double[] {11.20, 11.21, 11.22};
+        double[] calData = calories.stream().mapToDouble(i->i).toArray();
+
+        XYChart chart = QuickChart.getChart("Day vs. Calorie", "Date", "Calories", "Daily Trend", dateData, calData);
+
+        new SwingWrapper(chart).displayChart();
+        try{
+            BitmapEncoder.saveBitmap(chart, "./DailyTrend", BitmapEncoder.BitmapFormat.PNG);
+        } catch (IOException e){
+            System.out.printf("Found error: %s", e);
         }
     }
 
